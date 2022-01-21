33c60b1b3c055ce92d029e40a8baf2f542305f5c
Fmvmedeiros <84106516+Fmvmedeiros@users.noreply.github.com>
2022-01-21 10:04:43 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,28 +5,43 @@ bluetooth.onBluetoothConnected(function () {
 })
 bluetooth.onBluetoothDisconnected(function () {
     Ligado = false
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Asleep)
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
     if (cmd == "luz") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        if (Luz_esta_ligada) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            Luz_esta_ligada = false
+        } else {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            Luz_esta_ligada = true
+        }
     } else if (cmd == "vento") {
-        pins.digitalWritePin(DigitalPin.P2, 1)
+        if (Ventoinha_esta_ligada) {
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            Ventoinha_esta_ligada = false
+        } else {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            Ventoinha_esta_ligada = true
+        }
     } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.No)
     }
     game.addScore(1)
 })
 let cmd = ""
+let Ventoinha_esta_ligada = false
+let Luz_esta_ligada = false
 let Ligado = false
 Ligado = false
+Luz_esta_ligada = false
+Ventoinha_esta_ligada = false
 basic.showIcon(IconNames.Asleep)
 soundExpression.hello.play()
 basic.forever(function () {
     if (Ligado) {
-        bluetooth.uartWriteString(convertToText(pins.analogReadPin(AnalogPin.P1)))
+        bluetooth.uartWriteString("Luz:" + convertToText(pins.analogReadPin(AnalogPin.P1)) + "::  ::" + "Temp:" + convertToText(input.temperature()))
         basic.pause(100)
     }
 })
