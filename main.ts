2f3d9dcd061a9ca666399ee9a9b06e1d63dde7c9
Fmvmedeiros <84106516+Fmvmedeiros@users.noreply.github.com>
2022-01-21 12:30:05 +0000

    Updates

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function Luz_Automatica () {
+    if (Luz < Lmiar_Luz) {
+        Troca_Luz(false)
+    } else {
+        Troca_Luz(true)
+    }
+}
 bluetooth.onBluetoothConnected(function () {
     bluetooth.startUartService()
     Ligado = true
@@ -7,41 +14,64 @@ bluetooth.onBluetoothDisconnected(function () {
     Ligado = false
     basic.showIcon(IconNames.Asleep)
 })
+function Ventoinha_Automatica () {
+    if (Temperatura > Limiar_Temperatura) {
+        Troca_Ventoinha(false)
+    } else {
+        Troca_Ventoinha(true)
+    }
+}
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
     if (cmd == "luz") {
-        if (Luz_esta_ligada) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            Luz_esta_ligada = false
-        } else {
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            Luz_esta_ligada = true
-        }
+        Troca_Luz(Luz_esta_ligada)
     } else if (cmd == "vento") {
-        if (Ventoinha_esta_ligada) {
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            Ventoinha_esta_ligada = false
-        } else {
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            Ventoinha_esta_ligada = true
-        }
+        Troca_Ventoinha(Ventoinha_esta_ligada)
     } else {
         basic.showIcon(IconNames.No)
     }
     game.addScore(1)
 })
+function Troca_Ventoinha (Ventoinha_Ligada: boolean) {
+    if (Ventoinha_Ligada) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        Ventoinha_esta_ligada = false
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        Ventoinha_esta_ligada = true
+    }
+}
+function Troca_Luz (Luz_Ligada: boolean) {
+    if (Luz_Ligada) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        Luz_esta_ligada = false
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        Luz_esta_ligada = true
+    }
+}
 let cmd = ""
+let Temperatura = 0
+let Luz = 0
+let Lmiar_Luz = 0
+let Limiar_Temperatura = 0
 let Ventoinha_esta_ligada = false
 let Luz_esta_ligada = false
 let Ligado = false
 Ligado = false
 Luz_esta_ligada = false
 Ventoinha_esta_ligada = false
+Limiar_Temperatura = 23
+Lmiar_Luz = 800
 basic.showIcon(IconNames.Asleep)
 soundExpression.hello.play()
 basic.forever(function () {
+    Temperatura = input.temperature()
+    Luz = pins.analogReadPin(AnalogPin.P1)
+    Luz_Automatica()
+    Ventoinha_Automatica()
     if (Ligado) {
-        bluetooth.uartWriteString("Luz:" + convertToText(pins.analogReadPin(AnalogPin.P1)) + "::  ::" + "Temp:" + convertToText(input.temperature()))
-        basic.pause(100)
+        bluetooth.uartWriteString("Luz:" + convertToText(Luz) + "::  ::" + "Temp:" + convertToText(Temperatura))
     }
+    basic.pause(100)
 })
